4505d36acbe4ac8e169f33e20888894ac7373530
Joni Niemi <muvoksi@gmail.com>
2018-10-04 10:48:27 +0300

    First commit

---
+++ src/main/java/com/a1704471/lookingforgamer/LookingforgamerApplication.java
@@ -0,0 +1,12 @@
+package com.a1704471.lookingforgamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingforgamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookingforgamerApplication.class, args);
+	}
+}
