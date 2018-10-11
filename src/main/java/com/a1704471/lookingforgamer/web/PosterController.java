eb152cd4eb11262ea17cb757cdb4ea9db1494908
muvox <muvoksi@gmail.com>
2018-10-11 11:15:55 +0300

    Added functionality to add posters

--- src/main/java/com/a1704471/lookingforgamer/web/PosterController.java
+++ src/main/java/com/a1704471/lookingforgamer/web/PosterController.java
@@ -1,10 +1,12 @@
 package com.a1704471.lookingforgamer.web;
 
+import com.a1704471.lookingforgamer.domain.Poster;
 import com.a1704471.lookingforgamer.domain.PosterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PosterController {
@@ -15,5 +17,18 @@ public class PosterController {
     public String indexPage(Model model){
         model.addAttribute("posters", postRepo.findAll());
         return "index";
+    }
+
+    @RequestMapping(value= "/add")
+    public String addBook(Model model){
+        model.addAttribute("poster", new Poster());
+        model.addAttribute("categories", postRepo.findAll());
+        return "addposter";
+    }
+
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public String save(Poster poster){
+        postRepo.save(poster);
+        return "redirect:/";
     }
 }
